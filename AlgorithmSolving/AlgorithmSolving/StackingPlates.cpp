1f9db6547351c0b57e079784ad9bc633c43154e5
jkwchunjae <jkwchunjae@daum.net>
2014-04-15 00:11:32 +0900

    StackingPlates

--- AlgorithmSolving/AlgorithmSolving/StackingPlates.cpp
+++ AlgorithmSolving/AlgorithmSolving/StackingPlates.cpp
@@ -31,10 +31,12 @@ using namespace std;
 #define FOR1(v, lo, hi) for (int v=(lo);v<=(hi);v++)
 #define FORD1(v, lo, hi) for (int v=(hi);v>=(lo);v--)
 const double eps = 1 / (double)1000000000;
-const int INF = 1 << 30;
+//const int INF = 1 << 30;
+const int INF = 9999;
 
 int main(){
-	freopen("StackingPlates.in", "r", stdin);
+	//freopen("StackingPlates.in", "r", stdin);
+	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w+", stdout);
 	int stackCount;
 	int caseNumber = 0;
@@ -69,6 +71,10 @@ int main(){
 			dp[0][stackIndex] = (stack[stackIndex].find(list[0]) == stack[stackIndex].end()) ? INF : 0;
 		}
 
+		int checkMinNumber = -1;
+		int checkMinIndex = -1;
+
+
 		FOR(listIndex, 1, cntList)
 		{
 			int currNumber = list[listIndex];
@@ -89,19 +95,51 @@ int main(){
 					{
 						int add = (currNumber == *currStack.begin()) ? 1 : 2;
 						REP(j, stackCount) if (j != stackIndex) currDP = min(currDP, dp[listIndex - 1][j] + add);
+						if (currNumber == checkMinNumber && stackIndex == checkMinIndex && listIndex >= 2 && currNumber == list[listIndex - 2]) {
+							currDP = INF;
+						}
 					}
 				}
 			}
+
+			if (currNumber != checkMinNumber)
+			{
+				// 최소값이 하나일 경우에 대한 처리
+				// 최소값을 찾는다.
+				int minIndex = 0;
+				REP(i, stackCount) minIndex = (dp[listIndex][minIndex] > dp[listIndex][i]) ? i : minIndex;
+
+				// 최소값이 몇개인지 체크한다.
+				int cntMinn = 0;
+				REP(i, stackCount) cntMinn += (dp[listIndex][i] == dp[listIndex][minIndex]) ? 1 : 0;
+
+				// 최소값이 한개이면 골치아프다. 추가적인 조치가 필요
+				if (cntMinn == 1)
+				{
+					checkMinNumber = currNumber;
+					checkMinIndex = minIndex;
+				}
+				else
+				{
+					checkMinNumber = -1;
+					checkMinIndex = -1;
+				}
+			}
+
+		}
+
+		REP(i, cntList)
+		{
+			REP(j, stackCount)
+			{
+				//cout << dp[i][j] << '\t';
+			}
+			cout << list[i] << endl;
 		}
 		int result = INF;
 		REP(i, stackCount) result = min(result, dp[cntList - 1][i]);
 		//cout << "Case " << ++caseNumber << ": " << result << endl;
-		cout << result << endl;
-		if (result == 37)
-		{
-			result = 0;
-		}
+		//cout << result << endl;
 	}
 	return 0;
 }
-
