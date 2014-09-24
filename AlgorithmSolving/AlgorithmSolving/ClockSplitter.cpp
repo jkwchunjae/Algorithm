12f874b09e3bf40f223cb388198ae0de876c7108
jkwchunjae <jkwchunjae@daum.net>
2014-09-24 11:18:54 +0900

    ClockSplitter

--- AlgorithmSolving/AlgorithmSolving/ClockSplitter.cpp
+++ AlgorithmSolving/AlgorithmSolving/ClockSplitter.cpp
@@ -34,33 +34,44 @@ using namespace std;
 const double eps = 1 / (double)1000000000;
 const int INT__MIN = 1 << 31;
 const int INT__MAX = INT__MIN - 1;
-const long long LL_MIN = 1 << 63;
+const long long LL_MIN = 1LL << 63;
 const long long LL_MAX = LL_MIN - 1;
-
-typedef long long ll;
 
 int main(){
 #ifdef _DEBUG
-	freopen("input.txt", "r", stdin);
+	//freopen("input.txt", "r", stdin);
 	//freopen("output.txt", "w+", stdout);
 #endif
-	istream_iterator<int> input(cin), eof;
-	for_each(input, eof, [&](long long N)
+	long long N;
+	while (cin >> N)
 	{
-		cout << N << endl;
+		if (N == 0) break;
+
 		long long left = N / 4 + (N % 4 + 1) % 3;
 		long long right = N - N / 4 - (N % 4) / 2;
 		long long ans1 = N * (N + 1) / 4;
 		long long ans2 = N * (N + 1) / 2 - ans1;
-		long long leftSum = left * (left - 1) / 2;
-		long long a = 1;
-		long long b = right - ((2 * right + 1) - (long long)sqrt((2 * right + 1)*(2 * right + 1) - 8 * leftSum)) / 2;
-		long long rightSum = right * (right + 1) / 2 - b * (b - 1) / 2;
-		long long currSum = b * (b - 1) / 2;
-		long long begin = 1;
-		long long end = b - 1;
-		cout << left << " " << right << " " << ans1 << " " << ans2 << " " << leftSum << " " << b << endl;
-	});
+		long long currSum = ans1;
+		long long lastLeft = left;
+		long long lastRight = right;
+
+		while (left >= 1)
+		{
+			if (currSum == ans1 || currSum == ans2)
+			{
+				lastLeft = left;
+				lastRight = right;
+			}
+			if (currSum >= ans2)
+			{
+				currSum -= right;
+				right--;
+			}
+			left--;
+			currSum += left;
+		}
+		cout << max(1LL, lastLeft) << " " << lastRight << endl;
+	}
 	return 0;
 }
 
