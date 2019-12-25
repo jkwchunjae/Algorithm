002cfa5b2c3ffeee4002dd3ea933bf5e165414ea
Kyungwon Jeon <jkwchunjae@daum.net>
2019-12-25 15:25:50 +0900

    Use online input output

--- ProblemSolving/ProblemSolving/Program.cs
+++ ProblemSolving/ProblemSolving/Program.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+#if DEBUG // delete
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+#endif
 
 public class Program
 {
+#if DEBUG // delete
+    [STAThread]
+#endif
     public static void Main(string[] args)
+    {
+#if DEBUG // delete
+        var inputOutputList = BojUtils.MakeInputOutput("17969", useLocalInput: false);
+        foreach (var inputOutput in inputOutputList)
+        {
+            IO.SetInputOutput(inputOutput);
+#endif
+            Solve();
+#if DEBUG // delete
+            IO.IsCorrect().Dump();
+            Console.WriteLine();
+        }
+        DebugUtils.CopyCode();
+#endif
+    }
+
+    public static void Solve()
     {
         var nodeCount = IO.GetInt();
         var edgeList = new List<Edge>();
@@ -22,9 +48,6 @@ public class Program
         }
         var result = Solve(edgeList, nodeCount);
         result.Dump();
-#if DEBUG
-        IO.IsCorrect().Dump();
-#endif
     }
 
     public static long Solve(List<Edge> edgeList, int nodeCount)
@@ -139,7 +162,7 @@ public static class Extensionss
 
 public static class IO
 {
-#if DEBUG
+#if DEBUG // delete
     static List<string> _input;
     static string _answer;
     static string _output = "";
@@ -147,14 +170,27 @@ public static class IO
 
     public static bool IsCorrect() => _output.Trim() == _answer.Trim();
     public static int InputCount => _input.Count();
-#endif
+
     static IO()
     {
-#if DEBUG
         _input = File.ReadAllLines("input.txt", Encoding.UTF8).ToList();
         _answer = File.ReadAllText("output.txt", Encoding.UTF8);
-#endif
+        Init();
     }
+
+    public static void Init()
+    {
+        _output = "";
+        _readInputCount = 0;
+    }
+
+    public static void SetInputOutput(InputOutput inputOutput)
+    {
+        Init();
+        _input = inputOutput.Input.Replace("\r", "").Split('\n').ToList();
+        _answer = inputOutput.Output;
+    }
+#endif
 
     public static string GetLine()
     {
@@ -181,7 +217,7 @@ public static class IO
     {
         var text = string.IsNullOrEmpty(format) ? $"{obj}" : string.Format(format, obj);
         Console.WriteLine(text);
-#if DEBUG
+#if DEBUG // delete
         _output += Environment.NewLine + text;
 #endif
         return obj;
@@ -240,3 +276,166 @@ public static class Extensions
             action(item);
     }
 }
+
+#if DEBUG // delete
+
+public static class DebugUtils
+{
+    public static void CopyCode()
+    {
+        var programDirPath = Environment.CurrentDirectory;
+        while (!File.Exists(Path.Combine(programDirPath, "Program.cs")))
+        {
+            programDirPath = Directory.GetParent(programDirPath).FullName;
+        }
+        var programPath = Path.Combine(programDirPath, "Program.cs");
+        var sourceCode = File.ReadAllText(programPath);
+
+        var filteredCode = sourceCode
+            .DeleteDebug().DeleteDebug().DeleteDebug().DeleteDebug().DeleteDebug().DeleteDebug()
+            .DeleteDebug().DeleteDebug().DeleteDebug().DeleteDebug().DeleteDebug().DeleteDebug()
+            .DeleteDebug().DeleteDebug().DeleteDebug().DeleteDebug().DeleteDebug().DeleteDebug()
+            ;
+
+        Clipboard.SetText(filteredCode);
+    }
+
+    private static string DeleteDebug(this string sourceCode)
+    {
+        var beginIndex = sourceCode.IndexOf("#if DEBUG // delete");
+        if (beginIndex == -1)
+            return sourceCode;
+        var endIf = new List<string> { "#", "e", "n", "d", "i", "f" }.StringJoin("");
+        var endIndex = sourceCode.IndexOf(endIf, beginIndex);
+
+        return sourceCode.Substring(0, beginIndex - 1) + sourceCode.Substring(endIndex + 8);
+    }
+}
+
+#region Input Output
+
+public class InputOutput
+{
+    public int Number;
+    public string Input;
+    public string Output;
+}
+
+public static class BojUtils
+{
+    private static string GetHtml(this string problemNumber)
+    {
+        var client = new HttpClient();
+        var task = client.GetStringAsync($"https://www.acmicpc.net/problem/{problemNumber}");
+        task.Wait();
+        return task.Result;
+    }
+
+    public static List<InputOutput> MakeInputOutput(string problemNumber, bool useLocalInput = false)
+    {
+        if (useLocalInput)
+        {
+            return new List<InputOutput>
+            {
+                new InputOutput
+                {
+                    Number = 1,
+                    Input = File.ReadAllText("input.txt", Encoding.UTF8),
+                    Output = File.ReadAllText("output.txt", Encoding.UTF8),
+                },
+            };
+        }
+
+        if (AppCache.Exists(problemNumber))
+        {
+            return AppCache.GetCachedInputOutput(problemNumber);
+        }
+
+        var sampleDic = new Dictionary<(string, string), string>();
+
+        var sampleDataPattern = @"class\s*=\s*\""sampledata\""";
+        var sampleNumberPattern = @"id\s*=\s*\""sample-(input|output)-(\d+)\""";
+
+        var html = problemNumber.GetHtml();
+
+        var startIndex = 0;
+        while (true)
+        {
+            var a = html.IndexOf("<pre", startIndex);
+            if (a == -1)
+                break;
+            var b = html.IndexOf(">", a);
+            var c = html.IndexOf("</pre>", a);
+            var preHeader = html.Substring(a, b - a);
+            var preData = html.Substring(b + 1, c - b - 2).Trim();
+
+            // preHeader.Dump();
+            // preData.Dump();
+            if (!Regex.IsMatch(preHeader, sampleDataPattern))
+                continue;
+
+            var match = Regex.Match(preHeader, sampleNumberPattern);
+            if (match.Success)
+            {
+                var type = match.Groups[1].Captures[0].Value;
+                var number = match.Groups[2].Captures[0].Value;
+                // type.Dump();
+                // number.Dump();
+                sampleDic[(type, number)] = preData;
+            }
+
+            startIndex = a + 1;
+        }
+        // sampleDic.Dump();
+        var result = sampleDic
+            .GroupBy(x => new { Number = x.Key.Item2 })
+            .Select(x => new InputOutput
+            {
+                Number = int.Parse(x.Key.Number),
+                Input = x.First(e => e.Key.Item1 == "input").Value,
+                Output = x.First(e => e.Key.Item1 == "output").Value,
+            })
+            .ToList();
+
+        AppCache.SaveInputOutput(problemNumber, result);
+
+        return result;
+    }
+}
+
+public static class AppCache
+{
+    private static readonly string DirPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Boj");
+    public static bool Exists(string problemNumber)
+    {
+        var problemPath = Path.Combine(DirPath, $"{problemNumber}.json");
+        return File.Exists(problemPath);
+    }
+
+    public static List<InputOutput> GetCachedInputOutput(string problemNumber)
+    {
+        if (!Exists(problemNumber))
+            return null;
+
+        var problemPath = Path.Combine(DirPath, $"{problemNumber}.json");
+        var text = File.ReadAllText(problemPath, Encoding.UTF8);
+        return JsonConvert.DeserializeObject<List<InputOutput>>(text);
+    }
+
+    public static void SaveInputOutput(string problemNumber, List<InputOutput> data)
+    {
+        if (!Directory.Exists(DirPath))
+        {
+            Directory.CreateDirectory(DirPath);
+        }
+
+        var text = JsonConvert.SerializeObject(data, Formatting.Indented);
+        var problemPath = Path.Combine(DirPath, $"{problemNumber}.json");
+
+        File.WriteAllText(problemPath, text, Encoding.UTF8);
+    }
+}
+
+#endregion
+
+#endif
