8c6eab6b7f1454560680be2a3fa727917c90e4e6
jkw <jkwchunjae@daum.net>
2021-05-23 13:44:04 +0900

    C# 9.0

---
+++ ProblemSolving/WinFormsLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WinFormsLibrary1
+{
+    public static class Helper
+    {
+        public static void CopyText(string text)
+        {
+            Clipboard.SetText(text);
+        }
+    }
+}
