ffa8cedda0794dc7dfc2ad95c020843c050aefa3
Kyugnwon Jeon <jkwchunjae@daum.net>
2022-11-25 16:17:31 +0900

    https://leetcode.com/problems/sum-of-subarray-minimums/ 푸는 중

--- ProblemSolving/TestProject2/UnitTest1.cs
+++ ProblemSolving/TestProject2/UnitTest1.cs
@@ -5,58 +5,54 @@ namespace TestProject2;
 public class UnitTest1
 {
     [Fact]
-    public void CalculatorTest1()
+    public void SumSubMin1()
     {
-        var solution = new Solution();
-        var result = solution.Calculate("1 + 1");
-        Assert.Equal(2, result);
+        var arr = new[] { 5, 3, 1, 2, 4 };
+        var sol = new Solution();
+        sol.SumSubarrayMins(arr);
     }
 
-    [Fact]
-    public void CalculaorTest2()
-    { 
-        var solution = new Solution();
-        var result = solution.Calculate(" 2-1 + 2 ");
-        Assert.Equal(3, result);
-    }
+
+
+
+
+
+
+
 
     [Fact]
-    public void CalculatorTest3()
-    { 
-        var solution = new Solution();
-        var result = solution.Calculate("(1+(4+5+2)-3)+(6+8)");
-        Assert.Equal(23, result);
+    public void CountOfSubarray_1()
+    {
+        // [1, 2, 3] 중에 1이 포함되는 경우
+        var count = Ex.CountOfSubarray(3, 0);
+        Assert.Equal(3, count);
     }
-
     [Fact]
-    public void CalculatorTest4()
-    { 
-        var solution = new Solution();
-        var result = solution.Calculate("-1");
-        Assert.Equal(-1, result);
+    public void CountOfSubarray_2()
+    {
+        // [4, 1, 2, 3] 중에 1이 포함되는 경우
+        var count = Ex.CountOfSubarray(4, 1);
+        Assert.Equal(6, count);
     }
-
     [Fact]
-    public void CalculatorTest5()
-    { 
-        var solution = new Solution();
-        var result = solution.Calculate("-(2 + 3)");
-        Assert.Equal(-5, result);
+    public void CountOfSubarray_3()
+    {
+        // [5, 4, 1, 2, 3] 중에 1이 포함되는 경우
+        var count = Ex.CountOfSubarray(5, 2);
+        Assert.Equal(9, count);
     }
-
     [Fact]
-    public void CalculatorTest6()
-    { 
-        var solution = new Solution();
-        var result = solution.Calculate("-2+ 1");
-        Assert.Equal(-1, result);
+    public void CountOfSubarray_4()
+    {
+        // [6, 5, 4, 1, 2, 3] 중에 1이 포함되는 경우
+        var count = Ex.CountOfSubarray(6, 3);
+        Assert.Equal(12, count);
     }
-
     [Fact]
-    public void CalculatorTest7()
-    { 
-        var solution = new Solution();
-        var result = solution.Calculate("-(3+4)+5");
-        Assert.Equal(-2, result);
+    public void CountOfSubarray_5()
+    {
+        // [7, 6, 5, 4, 1, 2, 3] 중에 1이 포함되는 경우
+        var count = Ex.CountOfSubarray(7, 4);
+        Assert.Equal(15, count);
     }
 }
