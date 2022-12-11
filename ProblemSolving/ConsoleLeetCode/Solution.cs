86717a1d9996c0bc0880a1b214992e95023811c3
Kyungwon Jeon <jkwchunjae@daum.net>
2022-12-11 18:25:10 +0900

    [WA] https://leetcode.com/problems/binary-tree-maximum-path-sum/

--- ProblemSolving/ConsoleLeetCode/Solution.cs
+++ ProblemSolving/ConsoleLeetCode/Solution.cs
@@ -1,18 +1,53 @@
-﻿using System.Text;
+﻿namespace ConsoleLeetCode;
 
-namespace ConsoleLeetCode;
+public class TreeNode {
+    public int val;
+    public TreeNode? left;
+    public TreeNode? right;
+}
 
 public class Solution
 {
-    public string FrequencySort(string s)
+    public int MaxPathSum(TreeNode root)
     {
-        return s
-            .GroupBy(c => c)
-            .Select(x => new { Chr = x.Key, Count = x.Count() })
-            .OrderByDescending(x => x.Count)
-            .ThenBy(x => x.Chr)
-            .Aggregate(new StringBuilder(s.Length), (builder, item) => builder.Append(item.Chr, item.Count))
-            .ToString();
+        long maxResult = int.MinValue;
+
+        Queue<TreeNode> queue = new();
+        queue.Enqueue(root);
+        while (queue.Any())
+        {
+            var node = queue.Dequeue();
+            long pathSum = node.val;
+            pathSum += node.left?.SingleMax() ?? 0;
+            pathSum += node.right?.SingleMax() ?? 0;
+
+            maxResult = Math.Max(maxResult, pathSum);
+
+            if (node.left != null)
+                queue.Enqueue(node.left!);
+            if (node.right != null)
+                queue.Enqueue(node.right!);
+        }
+
+        return (int)maxResult;
     }
 }
 
+public static class Ex
+{
+    private static Dictionary<TreeNode, long> _singlemax = new();
+    public static long SingleMax(this TreeNode node)
+    {
+        if (_singlemax.TryGetValue(node, out var m))
+        {
+            return m;
+        }
+
+        var leftMax = node.left?.SingleMax() ?? 0;
+        var rightMax = node.right?.SingleMax() ?? 0;
+
+        var result = node.val + Math.Max(leftMax, rightMax);
+        _singlemax.Add(node, result);
+        return result;
+    }
+}
--- ProblemSolving/TestProject2/UnitTest1.cs
+++ ProblemSolving/TestProject2/UnitTest1.cs
@@ -4,15 +4,51 @@ namespace TestProject2;
 
 public class UnitTest1
 {
-    [Theory]
-    [InlineData("tree", "eert")]
-    [InlineData("cccaaa", "aaaccc")]
-    [InlineData("Aabb", "bbAa")]
-    public void Test(string input, string expected)
+    private TreeNode MakeTree(int?[] arr)
     {
-        var sol = new Solution();
-        var result = sol.FrequencySort(input);
+        Queue<(int index, TreeNode node)> queue = new();
+        var root = new TreeNode { val = arr[1]!.Value };
+        queue.Enqueue((1, root));
 
-        Assert.Equal(expected, result);
+        while (queue.Any())
+        {
+            var (index, node) = queue.Dequeue();
+            var leftIndex = index * 2;
+            var rightIndex = index * 2 + 1;
+            if (leftIndex < arr.Length && arr[leftIndex] != null)
+            {
+                var leftNode = new TreeNode { val = arr[leftIndex]!.Value };
+                node.left = leftNode;
+                queue.Enqueue((leftIndex, leftNode));
+            }
+            if (rightIndex < arr.Length && arr[rightIndex] != null)
+            {
+                var rightNode = new TreeNode { val = arr[rightIndex]!.Value };
+                node.right = rightNode;
+                queue.Enqueue((rightIndex, rightNode));
+            }
+        }
+
+        return root;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var tree = MakeTree(new int?[] { null, 1, 2, 3 });
+        var sol = new Solution();
+        var result = sol.MaxPathSum(tree);
+
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var tree = MakeTree(new int?[] { null, -10, 9, 20, null, null, 15, 7 });
+        var sol = new Solution();
+        var result = sol.MaxPathSum(tree);
+
+        Assert.Equal(42, result);
     }
 }
